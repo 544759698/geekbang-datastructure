e657439202d1054532cb0fa30bb84d8e1e86d8c4
yangguojun <544759698@qq.com>
2020-03-05 21:08:41 +0800

    day3

---
+++ src/main/java/com/yang/algorithm/day1/BestSolution.java
@@ -0,0 +1,34 @@
+package com.yang.algorithm.day1;
+
+/**
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ * <p>
+ * Created by yangguojun01 on 2020/3/5.
+ */
+public class BestSolution {
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {0, 0, 1, 1, 2, 3, 3, 5, 6};
+        int distinctCount = removeDuplicates(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+        System.out.println(distinctCount);
+    }
+
+}
