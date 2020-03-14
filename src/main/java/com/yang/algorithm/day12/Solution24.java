ca3e98bc1de80a3588b51ea173fcb849c91f20be
yangguojun <544759698@qq.com>
2020-03-14 12:45:01 +0800

    day12

---
+++ src/main/java/com/yang/algorithm/day12/Solution24.java
@@ -0,0 +1,30 @@
+package com.yang.algorithm.day12;
+
+/**
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ * <p>
+ * Created by yangguojun01 on 2020/3/14.
+ */
+public class Solution24 {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode ret = head.next;
+        ListNode pre = null;
+        ListNode tmp;
+        while (head != null && head.next != null) {
+            tmp = head.next;
+            head.next = head.next.next;
+            tmp.next = head;
+            if (pre != null) {
+                pre.next = tmp;
+            }
+            pre = head;
+            head = head.next;
+        }
+        return ret;
+    }
+
+}
