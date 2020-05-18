d20e6ef4d37428e45d7bce37aa027a72b61d9570
yangguojun <544759698@qq.com>
2020-05-18 22:57:41 +0800

    day37

---
+++ src/main/java/com/yang/algorithm/day37/ListNode.java
@@ -0,0 +1,14 @@
+package com.yang.algorithm.day37;
+
+/**
+ * Created by yangguojun01 on 2020/5/18.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
