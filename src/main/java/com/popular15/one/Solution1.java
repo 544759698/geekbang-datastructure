4a4547f0289167fd171290ccc58edab04e2e9a8f
yangguojun <544759698@qq.com>
2020-07-18 21:28:22 +0800

    popular15

---
+++ src/main/java/com/popular15/one/Solution1.java
@@ -0,0 +1,8 @@
+package com.popular15.one;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2020/7/19
+ */
+public class Solution1 {
+}
---
+++ src/main/java/com/popular15/two/Solution1.java
@@ -0,0 +1,8 @@
+package com.popular15.two;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2020/7/19
+ */
+public class Solution1 {
+}
